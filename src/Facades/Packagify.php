6705d463993333328075c26a40c479cf5abcc921
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2025-10-09 01:15:21 +0300

    Implement facade for `Packagify` package functionality.

---
+++ src/Facades/Packagify.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Larawise\Packagify\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Packagify
+ * @version     v0.0.1
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ *
+ * @see \Larawise\Packagify\Packagify
+ */
+class Packagify extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'packagify';
+    }
+}
