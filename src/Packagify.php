24aa44047af43094cea7865d10d64549b634795f
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2025-10-09 01:03:22 +0300

    Introduce abstract base class for Packagify packages.

---
+++ src/Packagify.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Larawise\Packagify;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Traits\Conditionable;
+use Illuminate\Support\Traits\Macroable;
+use Larawise\Packagify\Exceptions\PackagifyException;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Packagify
+ * @version     v0.0.1
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+class Packagify
+{
+  // ...
+}
