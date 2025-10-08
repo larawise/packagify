ade39313bc87d6e4f007a0d42b1264ba4d242f49
selcukcukur <selcukcukur@outlook.com.tr>
2025-10-09 02:23:52 +0300

    Preparations for discovering helpers.

---
+++ src/Discovery/Helpers.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Larawise\Packagify\Discovery;
+
+use Illuminate\Support\Facades\File;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Packagify
+ * @version     v0.0.1
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+trait Helpers
+{
+    /**
+     * Discover and manage package routes.
+     *
+     * @return void
+     */
+    protected function discoverHelpers()
+    {
+        // Check if the package has routes enabled.
+        if (! $this->package->options['hasHelpers']) {
+            return;
+        }
+
+        // Load helpers from the target helper path.
+        foreach ($this->package->helpers as $helper) {
+            if (File::exists($target = $this->path("helpers/$helper.php"))) {
+                File::requireOnce($target);
+            }
+        }
+    }
+}
