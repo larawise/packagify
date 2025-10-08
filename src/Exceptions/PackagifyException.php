38d465e3605ee09250fc4b8be174fa914680089b
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2025-10-09 01:01:13 +0300

    Implement custom exception class for `Packagify` errors.

---
+++ src/Exceptions/PackagifyException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Larawise\Packagify\Exceptions;
+
+use Exception;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Packagify
+ * @version     v0.0.1
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+class PackagifyException extends Exception
+{
+  // ...
+}
