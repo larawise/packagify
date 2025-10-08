3d413298493fd586ff6da9dbc74094c7e878dcf9
selcukcukur <selcukcukur@outlook.com.tr>
2025-10-09 02:22:55 +0300

    Preparations for discovering providers.

---
+++ src/Discovery/Providers.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Larawise\Packagify\Discovery;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Packagify
+ * @version     v0.0.1
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+trait Providers
+{
+    /**
+     * Discover and manage package providers.
+     *
+     * @return void
+     */
+    protected function discoverProviders()
+    {
+        // Check if the package has providers enabled.
+        if (! $this->package->options['hasProviders']) {
+            return;
+        }
+
+        // Register each provider in the package.
+        foreach ($this->package->providers as $provider) {
+            $this->app->register($provider);
+        }
+
+        // Determine the paths for the configurations within the package and publication path.
+        $path = $this->app->joinPaths($this->package->path, 'stubs/package');
+        $publish = config('packagify.paths.providers');
+
+        // If the application is running in the console, publish the configurations.
+        if ($this->app->runningInConsole()) {
+            $this->publishes([$path => $publish], "{$this->package->shortName()}-providers");
+        }
+    }
+}
