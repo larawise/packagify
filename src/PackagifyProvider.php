2ba074af0bf45b84b15bbd9158f7630d2c7ed142
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2025-10-09 01:11:50 +0300

    Add extendable service provider for `Packagify` packages.

---
+++ src/PackagifyProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Larawise\Packagify;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Packagify
+ * @version     v0.0.1
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+abstract class PackagifyProvider extends ServiceProvider
+{
+  // ...
+}
