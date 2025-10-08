8918abd4c8aaa82f087c3be17f8457f90e090bdf
selcukcukur <selcukcukur@outlook.com.tr>
2025-10-09 01:48:59 +0300

    Add custom command extending `package:discover` functionality.

---
+++ src/Commands/DiscoverCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Larawise\Packagify\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\PackageManifest;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Packagify
+ * @version     v0.0.1
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+#[AsCommand(name: 'package:discover', description: 'Rebuild the cached packagify and other package manifest')]
+class DiscoverCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'package:discover';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Rebuild the cached packagify and other package manifest.';
+
+    /**
+     * Execute the console command.
+     *
+     * @param PackageManifest $manifest
+     *
+     * @return void
+     */
+    public function handle(PackageManifest $manifest)
+    {
+        $packages = $this->packages($manifest);
+
+        $this->discoverPackages(
+            $packages, config('packagify.prefixes'), 'Packagify'
+        );
+
+        $this->discoverPackages(
+            $packages, config('packagify.prefixes'), 'Other', true
+        );
+    }
+
+    /**
+     * Discover packages and display them.
+     *
+     * @param Collection $packages
+     * @param array $prefixes
+     * @param string $type
+     * @param bool $inverse
+     *
+     * @return void
+     */
+    protected function discoverPackages($packages, $prefixes, $type, $inverse = false)
+    {
+        $this->components->info("[Packagify] Discovering `{$type}` packages");
+
+        $filter = $inverse
+            ? fn ($package) => ! Str::startsWith($package, $prefixes)
+            : fn ($package) => Str::startsWith($package, $prefixes);
+
+        $packages
+            ->filter($filter)
+            ->each(fn ($description) => $this->components->task($description))
+            ->whenNotEmpty(fn () => $this->newLine());
+    }
+
+    /**
+     * Get the packages for the manifest collection.
+     *
+     * @param PackageManifest $manifest
+     *
+     * @return Collection
+     */
+    protected function packages(PackageManifest $manifest)
+    {
+        $manifest->build();
+
+        return (new Collection($manifest->manifest))
+            ->keys();
+    }
+}
--- src/PackagifyServiceProvider.php
+++ src/PackagifyServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace Larawise\Packagify;
 
+use Illuminate\Foundation\Console\PackageDiscoverCommand;
+use Larawise\Packagify\Commands\DiscoverCommand;
+
 /**
  * Srylius - The ultimate symphony for technology architecture!
  *
@@ -27,5 +30,18 @@ class PackagifyServiceProvider extends PackagifyProvider
         $package->name('packagify')
             ->description('Packagify - Laravel packages made intuitive for every developer.')
             ->version('0.0.1');
+    }
+
+    /**
+     * Perform actions after the package is registered.
+     *
+     * @return void
+     */
+    public function packageRegistered()
+    {
+        // "Extend" a package discover command in the container.
+        $this->app->extend(
+            PackageDiscoverCommand::class, fn () => new DiscoverCommand
+        );
     }
 }
