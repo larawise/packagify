6f9ec0705be63ee9c7e82db5af0108156cf16f08
selcukcukur <selcukcukur@outlook.com.tr>
2026-01-09 22:21:34 +0300

    vip

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Larawise\Packagify\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
